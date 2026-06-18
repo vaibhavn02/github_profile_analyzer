58533d273a6bc5dde60acdd902869914a362e69f
vaibhavn02 <vaibhavn02@gmail.com>
2026-06-19 01:02:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.log('Error connecting database')
-    }
-    else {
-        console.log('Connected to MySQL database')
-    }
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection;
